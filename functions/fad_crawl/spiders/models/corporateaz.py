2b170dfdadeb307d80babcef559f90ebb51a4947
VAT <vietanhtran.96@gmail.com>
2020-05-24 21:58:06 -0500

    sprint 1 meeting 24-05

--- functions/fad_crawl/spiders/models/corporateaz.py
+++ functions/fad_crawl/spiders/models/corporateaz.py
@@ -1,5 +1,10 @@
 # Used for getting the list of all companies
 
+# catID: san giao dich (HOSE: 1, HNX: 2, etc.)
+# industryID: chon nganh (danh sach co the duoc lay tu request Industry List (category 1) tren Postman
+# businessTypeID: loai hinh doanh nghiep (danh sach lay tu request Business Type for Corporate A-Z) tren Postman
+# type: tabs (A-Z, Danh sach CK dang NY/GD, Niem yet moi/DKGD moi, etc.)
+
 import functions.fad_crawl.spiders.models.constants as constants
 
 data = {"url": "https://finance.vietstock.vn/data/corporateaz",
--- tasks.py
+++ tasks.py
@@ -2,6 +2,8 @@ from celery import Celery
 from functions.fad_crawl.spiders.main import crawl_test
 from functions.fad_crawl.spiders.main import crawl_main
 
+# use Redis for broker/backend
+
 app = Celery('tasks', broker='pyamqp://zerg:1234@localhost/zerg_vhost',
              backend='rpc://')
 
