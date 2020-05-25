dd1468db74bb70798a4db73dbd4dfe2519768950
Finance <finance@IBEW2085.local>
2020-05-25 15:17:48 -0500

    experimenting with downloaderMiddleware

--- functions/fad_crawl/settings.py
+++ functions/fad_crawl/settings.py
@@ -46,9 +46,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-   'fad_crawl.middlewares.FadCrawlSpiderMiddleware': 543,
-}
+# SPIDER_MIDDLEWARES = {
+#    'fad_crawl.middlewares.FadCrawlSpiderMiddleware': 542,
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
